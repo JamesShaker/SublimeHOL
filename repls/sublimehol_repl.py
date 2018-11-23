4ffb1d7c7e379bba7115320d41983b18c4bbfc30
James Shaker <james.shaker@gmail.com>
2018-11-23 11:26:01 +1100

    Some Changes

--- repls/sublimehol_repl.py
+++ repls/sublimehol_repl.py
@@ -14,12 +14,11 @@ class SublimeHOLRepl(SubprocessRepl):
    
     def write(self, command):
         #strip the command of terms and strings and comments
-        stripped_command = re.sub('“([\w\s\S]*?)”','',command)
-        stripped_command = re.sub('‘([\w\s\S]*?)’','',command)
+        stripped_command = re.sub('\`\`([\w\s\S]*?)\`\`','',stripped_command)
         stripped_command = re.sub('\`([\w\s\S]*?)\`','',stripped_command)
-        stripped_command = re.sub('\"([\w\s\S]*?)\"','',stripped_command)
         stripped_command = re.sub('\“([\w\s\S]*?)\”','',stripped_command)
         stripped_command = re.sub('\‘([\w\s\S]*?)\’','',stripped_command)
+        stripped_command = re.sub('\"([\w\s\S]*?)\"','',stripped_command)
         stripped_command = re.sub('\(\*([\w\s\S]*?)\*\)','',stripped_command)
 
         #get open dependencies
@@ -49,5 +48,4 @@ class SublimeHOLRepl(SubprocessRepl):
         if self.is_alive():
             #Need to send signal to children because HOL runs everything
             #in build heap
-            print("Trying to get group of " + str(self.popen.pid) + " killed with signal " + str(sig))
             os.killpg(os.getpgid(self.popen.pid), sig)
